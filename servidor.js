202f4cce40977ac5007f34ea5a4c058a8f05105f
Diego233 <chacond233@gmail.com>
2018-11-10 12:50:37 -0500

    actualizacion servidor

--- servidor.js
+++ servidor.js
@@ -1,14 +1,12 @@
-const http = require('http');
+// app.js
+var hostname = '127.0.0.1';
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 5000
 
-const hostname = '127.0.0.1';
-/*const port = 3000;*/
-const port = process.env.PORT || 5000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo\n');
-});
+app.get('/', function(req, res) {
+  res.send('Hola Mundo!')
+})
 
 server.listen(port, hostname, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
